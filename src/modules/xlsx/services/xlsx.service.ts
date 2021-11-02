3a80ce31db453be099a9ea04277c4efb78f9d034
gabriel-roque <gabrielrms.dll@gmail.com>
2021-11-02 11:41:33 -0300

    refeactor: abstract operations export

--- src/modules/xlsx/services/xlsx.service.ts
+++ src/modules/xlsx/services/xlsx.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 import { FileService } from 'src/modules/file/services/file.service';
 
+import { Column, Row, Tab } from 'src/dto/common.input';
 import { XlsxDataInput } from 'src/dto/xlsx.input';
 
 import * as dayjs from 'dayjs';
@@ -14,18 +15,18 @@ export class XlsxService {
 
   constructor(private readonly fileService: FileService) {}
 
-  private async createWorkBook(data: XlsxDataInput): Promise<void> {
+  private async createWorkBookTemplate(data: XlsxDataInput): Promise<void> {
     const workbook = new ExcelJS.Workbook();
     this.workbook = await workbook.xlsx.readFile('static/template.xlsx');
     const presensationSheet = this.workbook.getWorksheet(1);
 
-    workbook.title = 'Jazida Export';
-    workbook.subject = data.author.email;
-    workbook.created = new Date();
-    workbook.creator = 'Jazida.com';
-    workbook.description = `Exportação gerada por ${data.author.name} - ${data.author.email}`;
-    workbook.lastModifiedBy = 'Jazida Exportação <jazida-export>';
-    workbook.manager = 'Jazida Exportação <jazida-export>';
+    this.workbook.title = 'Jazida Export';
+    this.workbook.subject = data.author.email;
+    this.workbook.created = new Date();
+    this.workbook.creator = 'Jazida.com';
+    this.workbook.description = `Exportação gerada por ${data.author.name} - ${data.author.email}`;
+    this.workbook.lastModifiedBy = 'Jazida Exportação <jazida-export>';
+    this.workbook.manager = 'Jazida Exportação <jazida-export>';
 
     if (data.company) {
       const companyCell = presensationSheet.getCell('G9');
@@ -46,57 +47,23 @@ export class XlsxService {
   }
 
   async export(data: XlsxDataInput): Promise<string> {
-    await this.createWorkBook(data);
+    await this.createWorkBookTemplate(data);
 
     data.tabs.forEach((tab) => {
-      const sheet = this.workbook.addWorksheet(tab.name, {
-        properties: { tabColor: { argb: tab?.color } },
-      });
+      const sheet = this.createWorkSheet(tab);
 
-      sheet.columns = tab.columns.map((column) => {
-        return {
-          key: column.key,
-          header: column.value,
-          width: column.width || 20,
-        };
-      });
-
-      sheet.columns.forEach((col) => {
-        col.eachCell((cell) => {
-          const collumnCell = sheet.getCell(cell.address);
-
-          collumnCell.fill = {
-            type: 'pattern',
-            pattern: 'solid',
-            fgColor: { argb: 'FF00B365' },
-          };
-
-          collumnCell.font = {
-            bold: true,
-            name: 'Arial',
-            color: { argb: 'FFFFFFFF' },
-          };
-        });
-      });
+      sheet.columns = this.setCollumns(tab.columns);
+      this.setStylesCollumns(sheet);
 
       tab.rows.forEach((row, i) => {
-        let objectRow = {};
-        Object.keys(row).map((prop) => {
-          objectRow = { [prop]: row[prop].value, ...objectRow };
-        });
-
+        const objectRow = this.mapObjectRow(row);
         const rowInserted = sheet.insertRow(i + 2, { ...objectRow });
 
         Object.keys(row).forEach((prop) => {
           rowInserted.model.cells.forEach((cell) => {
             if (row[prop].value === cell.value) {
-              if (row[prop].color) {
-                sheet.getCell(String(cell.address)).fill = {
-                  pattern: 'solid',
-                  type: 'pattern',
-                  fgColor: { argb: row[prop].color },
-                };
-              }
+              if (row[prop].color)
+                this.setColorCell(sheet, cell, row[prop].color);
             }
           });
         });
@@ -109,5 +76,61 @@ export class XlsxService {
 
     await this.fileService.writeStream(fileName, buffer);
     return fileName;
+  }
+
+  private createWorkSheet(tab: Tab): ExcelJS.Worksheet {
+    return this.workbook.addWorksheet(tab.name, {
+      properties: { tabColor: { argb: tab?.color } },
+    });
+  }
+
+  private mapObjectRow(row: Row): any {
+    let objectRow = {};
+    Object.keys(row).map((prop) => {
+      objectRow = { [prop]: row[prop].value, ...objectRow };
+    });
+    return objectRow;
+  }
+
+  private setCollumns(columns: Column[]): Partial<ExcelJS.Column>[] {
+    return columns.map((column) => {
+      return {
+        key: column.key,
+        header: column.value,
+        width: column.width || 20,
+      };
+    });
+  }
+
+  private setStylesCollumns(sheet: ExcelJS.Worksheet): void {
+    sheet.columns.forEach((col) => {
+      col.eachCell((cell) => {
+        const collumnCell = sheet.getCell(cell.address);
+
+        collumnCell.fill = {
+          type: 'pattern',
+          pattern: 'solid',
+          fgColor: { argb: 'FF00B365' },
+        };
+
+        collumnCell.font = {
+          bold: true,
+          name: 'Arial',
+          color: { argb: 'FFFFFFFF' },
+        };
+      });
+    });
+  }
+
+  private setColorCell(
+    sheet: ExcelJS.Worksheet,
+    cell: ExcelJS.CellModel,
+    color: string,
+  ): void {
+    sheet.getCell(String(cell.address)).fill = {
+      pattern: 'solid',
+      type: 'pattern',
+      fgColor: { argb: color },
+    };
   }
 }
